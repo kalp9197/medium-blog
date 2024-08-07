ea16769a0aa7970aab9cd9e3bded3f81272af34b
Kalp Patel <kalppatel@Kalps-MacBook-Air.local>
2024-08-07 15:49:46 +0530

    Adding signin,signup routes auth and jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,71 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
+app.get("/", (c) => {
+  return c.text("hellooworld");
+});
 
-app.post('/api/v1/signup',(c) => {
-  return c.text('hello')
-})
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.post('/api/v1/signin',(c) => {
-  return c.text('hello')
-})
+  const body = await c.req.json();
 
-app.post('/api/v1/blog',(c) => {
-  return c.text('hello')
-})
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
 
-app.put('/api/v1/blog',(c) => {
-  return c.text('hello')
-})
+  //@ts-ignore
+  const token = await sign({ id: user.id }, "kalpsecret");
 
-app.get('/api/v1/blog/:id',(c) => {
-  return c.text('hello')
-})
+  return c.json({
+    jwt: token,
+  });
+});
 
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
 
-export default app
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "user not found" });
+  }
+
+  //@ts-ignore
+  const jwt = await sign({ id: user.id }, "kalpsecret");
+  return c.json({ jwt });
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("hello");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("hello");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("hello");
+});
+
+export default app;
